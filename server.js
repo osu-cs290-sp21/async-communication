a4b06ff031dec71fc678aa0ab22f8fd865ed78b0
Rob Hess <hessro@oregonstate.edu>
2021-06-01 15:41:20 -0700

    Add code from 6/1 lecture.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var fs = require('fs')
 
 var peopleData = require('./peopleData');
 
@@ -9,6 +10,7 @@ var port = process.env.PORT || 8000;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(express.json())
 app.use(express.static('public'));
 
 app.get('/', function (req, res, next) {
@@ -29,6 +31,35 @@ app.get('/people/:person', function (req, res, next) {
     next();
   }
 });
+
+app.post('/people/:person/addPhoto', function (req, res, next) {
+  console.log("== req.body:", req.body)
+  if (req.body && req.body.url && req.body.caption) {
+    var person = req.params.person.toLowerCase();
+    if (peopleData[person]) {
+      peopleData[person].photos.push({
+        url: req.body.url,
+        caption: req.body.caption
+      })
+      console.log("== peopleData[" + person +"]:", peopleData[person])
+      fs.writeFile(
+        __dirname + '/peopleData.json',
+        JSON.stringify(peopleData, null, 2),
+        function (err) {
+          if (err) {
+            res.status(500).send("Error writing new data.  Try again later.")
+          } else {
+            res.status(200).send()
+          }
+        }
+      )
+    } else {
+      next()
+    }
+  } else {
+    res.status(400).send("Request needs a JSON body with 'url' and 'caption'.")
+  }
+})
 
 app.get('*', function (req, res, next) {
   res.status(404).render('404', {
